d55816f4aa81853cf75e881c7f72c4a3b3fac40d
Lakshay <lakshayd2002@gmail.com>
2025-05-09 15:56:51 -0400

    Added functions to do gradient descent and compare it with regular regression.

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ df = pd.read_csv(directory, index_col=False)
 x = df['YearsExperience'].to_numpy()
 y = df['Salary'].to_numpy()
 
-
 def linear_regression(x, y):
     x_mean = np.mean(x)
     y_mean = np.mean(y)
@@ -26,24 +25,58 @@ def linear_regression(x, y):
     return m, b
 
 
-def predict(m, x, b):
+def predict(x, m, b):
     return m * x + b
 
 
 def rmse(y_true, y_pred):
     return np.sqrt(np.mean((y_true - y_pred) ** 2))
 
+def cost(m, b, x, y):
+    n = len(x)
+    cost = 1/(2*n)*(np.sum((m * x + b - y)**2))
+    return cost
 
-m, b = linear_regression(x, y)
+def partiald_m(m, b, x, y):
+    n = len(x)
+    partiald_m = (1/n) * (np.sum((m * x + b - y) * x))
+    return partiald_m
 
-predictions = predict(x, m, b)
+def partiald_b(m, b, x, y):
+    n = len(x)
+    partiald_b = (1/n) * (np.sum(m * x + b -y))
+    return partiald_b
 
-error = rmse(y, predictions)
+def descent(alpha, m, b, x, y, iterations):
+    cost_history = []
+    i = 0
+    while i < iterations:
+        m = m - alpha * partiald_m(m, b, x, y)
+        b = b - alpha * partiald_b(m, b, x, y)
+        i += 1
+        cost_history.append(cost(m, b, x, y))
+    return m, b, cost_history
 
-print("Slope (m):", m)
-print("Intercept (b):", b)
-print("Predictions:", predictions)
-print("RMSE:", error)
+m, b = 0.0, 0.0
+alpha = 0.005
+iterations = 10000
+
+m_gd, b_gd = descent(alpha, m, b, x, y, iterations)
+
+m_cf, b_cf = linear_regression(x, y)
+
+y_pred_cf = predict(x, m_cf, b_cf)
+y_pred_gd = predict(x, m_gd, b_gd)
+
+error_cf = rmse(y, y_pred_cf)
+error_gd = rmse(y, y_pred_gd)
+
+print(f"GD slope: {m_gd:.2f}, GD intercept: {b_gd:.2f}")
+print(f"CF slope: {m_cf:.2f}, CF intercept: {b_cf:.2f}")
+print("Predictions:", y_pred_cf)
+print("Closed-form RMSE:", error_cf)
+print("Gradient Descent RMSE:", error_gd)
+
 
 print("--- DataFrame ---")
 print(df.to_string(index=False))
@@ -51,10 +84,14 @@ print(df.to_string(index=False))
 
 plt.scatter(x, y, label="Data")
 sorted_idx = np.argsort(x)
-plt.plot(x[sorted_idx], predictions[sorted_idx], linewidth=2, label='Fit')
+plt.plot(x[sorted_idx], y_pred_cf[sorted_idx], linewidth=2, label='Fit')
 plt.xlabel("Years of Experience")
 plt.ylabel("Salary")
 plt.title("Salary vs. Years of Experience")
 plt.grid(True)
 plt.show()
 
+# for α in [0.0005, 0.001, 0.005]:
+#     m_gd, b_gd, costs = descent(α, 0, 0, x, y, 1000)
+#     plt.plot(costs, label=f'α={α}')
+# plt.legend(); plt.show()
